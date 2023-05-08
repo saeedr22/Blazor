4c6554759c0bffb25f07dfae707533d55e1748e9
Saeed Rezaei <saeed.rezaei1@gmail.com>
2023-05-08 16:01:28 +0430

    init

---
+++ DataBinding/Models/BlazorRoom.cs
@@ -0,0 +1,12 @@
+﻿namespace DataBinding.Models
+{
+    public class BlazorRoom
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public decimal Price { set; get; }
+        public bool IsActive { set; get; }
+        public List<BlazorRoomProp> RoomProps { set; get; }
+
+    }
+}
